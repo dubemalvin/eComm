e510637ffacdf8edb3e92af5fcd7c76fce463c87
malvindube <dubemalvin024@gmail.com>
2024-10-08 12:42:48 +0200

    Revert "First commit on the Commence Backend"
    This reverts commit 397c57d3

--- src/main/java/com/malvin/EComm/exception/AlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.malvin.EComm.exception;
-
-public class AlreadyExistsException extends RuntimeException {
-    public AlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/malvin/EComm/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.malvin.EComm.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
